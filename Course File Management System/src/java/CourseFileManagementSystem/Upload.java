d08086d04fd13f574f4e8e1e247581ec77a3fa3c
Aidan Toh <zavier_mx@live.com.my>
2015-07-30 18:54:49 +0800

    Upload function is done

---
+++ Course File Management System/src/java/CourseFileManagementSystem/Upload.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CourseFileManagementSystem;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+/**
+ *
+ * @author zavie_000
+ */
+@WebServlet(name = "Upload", urlPatterns = {"/Upload"})
+public class Upload extends HttpServlet 
+{
+    private static final long serialVersionUID = 1L;
+    private static final String DATA_DIRECTORY = "data";
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_MEMORY_SIZE = 1024 * 1024 * 5; //5MB
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50; //50MB
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        // Check that we have a file upload request
+        if (!ServletFileUpload.isMultipartContent(request)) 
+        {
+            // if not, we stop here
+            PrintWriter writer = response.getWriter();
+            writer.println("Error: Form must has enctype=multipart/form-data.");
+            writer.flush();
+            return;
+        }
+       
+        // Create a factory for disk-based file items
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        // Sets the size threshold beyond which files are written directly to
+        // disk.
+        factory.setSizeThreshold(MAX_MEMORY_SIZE);
+
+        // Sets the directory used to temporarily store files that are larger
+        // than the configured size threshold. We use temporary directory for
+        // java
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        // constructs the folder where uploaded file will be stored
+        String uploadFolder = getServletContext().getRealPath("") + File.separator + DATA_DIRECTORY;
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        
+        // Sets maximum size of upload file
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+        
+        // Set overall request size constraint
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+
+        // creates the directory if it does not exist
+        File uploadDir = new File(uploadFolder);
+        if (!uploadDir.exists()) 
+        {
+            uploadDir.mkdir();
+        }
+        
+        try 
+        {
+            // Parse the request
+            List<FileItem> items = upload.parseRequest(request);
+            if (items != null && items.size() > 0) 
+            {
+                // iterates over form's fields
+                for (FileItem item : items) 
+                {
+                    // processes only fields that are not form fields
+                    if (!item.isFormField()) 
+                    {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadFolder + File.separator + fileName;
+                        File uploadedFile = new File(filePath);
+                        System.out.println(filePath);
+                          
+                        // saves the file to upload directory
+                        item.write(uploadedFile); 
+                    }
+                }    
+            }
+            // displays done.jsp page after upload finished
+            //getServletContext().getRequestDispatcher("/Course_File_Management_System/done.jsp").forward(request, response);
+        } 
+        catch (FileUploadException ex) 
+        {
+            throw new ServletException(ex);
+        } 
+        catch (Exception ex) 
+        {
+            throw new ServletException(ex);
+        }
+    }
+}
