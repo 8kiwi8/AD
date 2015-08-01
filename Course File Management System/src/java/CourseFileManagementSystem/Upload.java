13096bd730f6733617a2b0271e0ace384a7e04b6
Aidan Toh <zavier_mx@live.com.my>
2015-08-01 14:14:33 +0800

    Updated upload function

--- Course File Management System/src/java/CourseFileManagementSystem/Upload.java
+++ Course File Management System/src/java/CourseFileManagementSystem/Upload.java
@@ -6,11 +6,19 @@
 package CourseFileManagementSystem;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.List;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletContext;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,16 +35,22 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 @WebServlet(name = "Upload", urlPatterns = {"/Upload"})
 public class Upload extends HttpServlet 
 {
+    private final String dbURL = "jdbc:mysql://localhost:3306/cfms";
+    private final String dbUser = "root";
+    private final String dbPass = "";
+    
     private static final long serialVersionUID = 1L;
     private static final String DATA_DIRECTORY = "data";
     private static final int MAX_FILE_SIZE = 1024 * 1024 * 40; // 40MB
     private static final int MAX_MEMORY_SIZE = 1024 * 1024 * 5; //5MB
     private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50; //50MB
-
+    String fileName = "";
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.write("<html><head></head><body>");
         // Check that we have a file upload request
         if (!ServletFileUpload.isMultipartContent(request)) 
         {
@@ -78,8 +92,14 @@ public class Upload extends HttpServlet
             uploadDir.mkdir();
         }
         
+        Connection conn = null; // connection to the database
+        
         try 
         {
+            // connects to the database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            
             // Parse the request
             List<FileItem> items = upload.parseRequest(request);
             if (items != null && items.size() > 0) 
@@ -90,18 +110,23 @@ public class Upload extends HttpServlet
                     // processes only fields that are not form fields
                     if (!item.isFormField()) 
                     {
-                        String fileName = new File(item.getName()).getName();
+                        fileName = new File(item.getName()).getName();
                         String filePath = uploadFolder + File.separator + fileName;
                         File uploadedFile = new File(filePath);
                         System.out.println(filePath);
                           
                         // saves the file to upload directory
                         item.write(uploadedFile); 
+                        PreparedStatement statement = conn.prepareStatement("INSERT INTO files (filesDir) values(?)");
+                        statement.setString(1, filePath);
+                        statement.executeUpdate();
+                                                                     
+                        out.write("File "+ item.getName() +" uploaded successfully.");
+                        out.write("<br><br>");
+                        out.write("<a href=\"Upload?filePath="+ filePath + "\">Download "+ item.getName()+"</a>");                       
                     }
                 }    
-            }
-            // displays done.jsp page after upload finished
-            //getServletContext().getRequestDispatcher("/Course_File_Management_System/done.jsp").forward(request, response);
+            }               
         } 
         catch (FileUploadException ex) 
         {
@@ -110,6 +135,49 @@ public class Upload extends HttpServlet
         catch (Exception ex) 
         {
             throw new ServletException(ex);
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        String filePath = request.getParameter("filePath");
+
+        if(filePath == null || filePath.equals(""))
+        {
+            throw new ServletException("File Name can't be null or empty");
+        }
+
+        File file = new File(filePath);
+
+        if(!file.exists())
+        {
+            throw new ServletException("File doesn't exists on server.");
+        }
+
+        ServletContext ctx = getServletContext();
+
+        try (InputStream fis = new FileInputStream(file))
+        {
+            String mimeType = ctx.getMimeType(file.getAbsolutePath());
+            
+            response.setContentType(mimeType != null? mimeType:"application/octet-stream");
+            response.setContentLength((int) file.length());
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+            
+            try (ServletOutputStream os = response.getOutputStream()) 
+            {
+                byte[] bufferData = new byte[1024];
+                int read = 0;
+                
+                while((read = fis.read(bufferData))!= -1)
+                {
+                    os.write(bufferData, 0, read);
+                }
+                os.flush();
+                os.close();
+                fis.close();
+            }
         }
     }
 }
